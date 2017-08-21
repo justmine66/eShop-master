9e3e747b5b9d15e494386596457865ecd6064acf
Administrator <Administrator@JUSTMINE-PC>
2017-08-21 11:11:35 +0800

    1

---
+++ src/Services/Ordering/Ordering.API/Application/Queries/IOrderQueries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Application.Queries
+{
+    /// <summary>
+    /// 订单查询接口
+    /// </summary>
+    public interface IOrderQueries
+    {
+        Task<dynamic> GetOrderAsync(int id);
+        Task<IEnumerable<dynamic>> GetOrdersAsync();
+        Task<IEnumerable<dynamic>> GetCardTypesAsync();
+    }
+}
---
+++ src/Services/Ordering/Ordering.API/Application/Queries/OrderQueries.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Application.Queries
+{
+    /// <summary>
+    /// 订单查询
+    /// </summary>
+    public class OrderQueries
+        : IOrderQueries
+    {
+        private string _connectionString = string.Empty;
+
+        public OrderQueries(string conStr)
+        {
+            _connectionString = !string.IsNullOrWhiteSpace(conStr)
+                ? conStr
+                : throw new ArgumentNullException(nameof(conStr));
+        }
+
+        public Task<IEnumerable<dynamic>> GetCardTypesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetOrderAsync(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var result = connection.QueryAsync<dynamic>(
+                    @"",
+                    new { id });
+
+                if (result.AsList())
+                {
+
+                }
+            }
+        }
+
+        public Task<IEnumerable<dynamic>> GetOrdersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        //映射订单项
+        private dynamic MapOrderItems(dynamic result)
+        {
+            dynamic order = new ExpandoObject();
+
+            return order;
+        }
+    }
+}
