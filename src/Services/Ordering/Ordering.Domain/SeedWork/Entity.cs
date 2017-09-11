d5fe9c056f5589f2088db041a2ab4a6256e628d9
justmine <justmine@DESKTOP-UILU6SC>
2017-09-11 06:24:40 -0700

    1

--- src/Services/Ordering/Ordering.Domain/SeedWork/Entity.cs
+++ src/Services/Ordering/Ordering.Domain/SeedWork/Entity.cs
@@ -6,14 +6,14 @@ using System.Text;
 namespace Ordering.Domain.SeedWork
 {
     /// <summary>
-    /// 实体抽象
+    /// 实体
     /// </summary>
     public abstract class Entity
     {
-        int? _requestHashCode;
-        int _Id;
+        int? _requestHashCode;//所请求哈希码
+        int _Id;//标识
 
-        private List<INotification> _domainEvents;
+        private List<INotification> _domainEvents;//领域事件字段
 
         public virtual int Id
         {
@@ -21,6 +21,7 @@ namespace Ordering.Domain.SeedWork
             protected set { this._Id = value; }
         }
 
+        //领域事件操作
         public List<INotification> DomainEvents => this._domainEvents;
         public void AddDomainEvent(INotification eventItem)
         {
@@ -42,28 +43,33 @@ namespace Ordering.Domain.SeedWork
             return this._Id == default(int);
         }
 
+        /// <summary>
+        /// 等于,标识相等即代表两个实体相等.
+        /// </summary>
+        /// <param name="obj">实体,派生自Entity</param>
+        /// <returns>是否相等</returns>
         public override bool Equals(object obj)
         {
             if (null == obj || !(obj is Entity))
                 return false;
 
-            if (object.ReferenceEquals(this, obj))
+            if (object.ReferenceEquals(this, obj))//引用相等
                 return true;
 
             if (obj.GetType() != this.GetType())
                 return false;
 
-            Entity item = obj as Entity;
+            var item = obj as Entity;
 
             if (item.IsTransient() || this.IsTransient())
                 return false;
             else
-                return item.Id == this.Id;
+                return item.Id == this.Id;//标识相等即代表两个实体相等.
         }
 
         public override int GetHashCode()
         {
-            if (this.IsTransient())//Id为空
+            if (this.IsTransient())//实体为空
                 return base.GetHashCode();
             else
             {
