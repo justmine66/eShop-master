e74784e84b43bcb9b54dc51f6491ddab7ae09212
Administrator <Administrator@JUSTMINE-PC>
2017-12-13 15:31:05 +0800

    1

---
+++ src/Services/Basket/Basket.API/Models/BasketItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Models
+{
+    public class BasketItem : IValidatableObject
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal OldUnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string PictureUrl { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
